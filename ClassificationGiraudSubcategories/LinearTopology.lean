2f54ee7b276e92d83408b0de854ea2ba02291d44
blake-farman <blake.farman@gmail.com>
2025-12-07 09:24:46 -0600

    Removed the Ring namespace

--- ClassificationGiraudSubcategories/LinearTopology.lean
+++ ClassificationGiraudSubcategories/LinearTopology.lean
@@ -3,11 +3,6 @@ import Mathlib.RingTheory.Ideal.Colon
 
 variable (A : Type*) [Ring A]
 
-namespace Ring
-variable (I : RightIdeal A)
-
 structure RightLinearTopology extends IdealFilter A where
   (colon_closed : ∀ {I : RightIdeal A} (a : A),
     I ∈ sets → (I.colon (RightIdeal.span {a})) ∈ sets)
-
-end Ring
